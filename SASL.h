cf1228599700787548803e873d0219656afe0f82
Rockford Wei <rocky@perfect.org>
2017-01-25 15:04:31 -0500

    Exporting More Symbols.

--- SASL.h
+++ SASL.h
@@ -1,9 +1,13 @@
 #ifndef __PERFECT_SASL__
 #define __PERFECT_SASL__
 #ifdef __APPLE__
+#include "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/sasl/gai.h"
 #include "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/sasl/sasl.h"
+#include "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/sasl/saslplug.h"
+#include "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/sasl/saslutil.h"
 #else
 #include "/usr/include/sasl/sasl.h"
+#include "/usr/include/sasl/saslplug.h"
+#include "/usr/include/sasl/saslutil.h"
 #endif
-
 #endif
